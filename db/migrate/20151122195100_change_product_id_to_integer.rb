b65e9e64d4e2d208601794118cf70a1f019e02d4
Tim Chai <timothy.y.chai@gmail.com>
2015-11-25 17:49:37 -0800

    commit message

--- db/migrate/20151122195100_change_product_id_to_integer.rb
+++ db/migrate/20151122195100_change_product_id_to_integer.rb
@@ -1,5 +1,5 @@
 class ChangeProductIdToInteger < ActiveRecord::Migration
   def change
-    change_column :images, :product_id, :integer
+    change_column :images, :product_id, 'integer USING (trim(price)::integer)'
   end
 end
